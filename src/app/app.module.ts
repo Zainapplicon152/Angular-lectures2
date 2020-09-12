0ce25ffd3163c9a35bf4adccf7ea17750ccbaecd
Zain Ul Abideen <66625704+Zainapplicon152@users.noreply.github.com>
2020-09-11 20:07:52 -0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserModule } from "./user/user.module";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AdminModule } from "./admin/admin.module";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    AdminModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
